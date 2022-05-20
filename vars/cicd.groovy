9ba459af89561561bfce42746249e209e9c26528
bairusrinivas <104980931+bairusrinivas@users.noreply.github.com>
2022-05-20 10:44:52 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -10,3 +10,7 @@ def newDeploy(ip,contextpath)
 {
   "scp /home/ubuntu/.jenkins/workspace/Declerativepipelinewithsharedlibrary/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${contextpath}.war"
 }
+def runSelenium(path)
+{
+  sh "java -jar $path/testing.jar"
+}
